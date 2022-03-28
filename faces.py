8e95f9d62539c1ceba95f441ae6b287fe3a1b167
AlicjaKasicka <alice55451@gmail.com>
2022-03-28 22:31:58 +0200

    Left eye detection

--- faces.py
+++ faces.py
@@ -1,12 +1,12 @@
 import cv2
 
-cascade = cv2.CascadeClassifier("Cascade.xml")
+cascade = cv2.CascadeClassifier("LeftEyeCascade.xml")
 image1 = cv2.imread("face1.jpg")
 gray_image = cv2.cvtColor(image1, cv2.COLOR_BGR2GRAY)
 
-cv2.imshow("image", gray_image)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+#cv2.imshow("image", gray_image)
+#cv2.waitKey(0)
+#cv2.destroyAllWindows()
 
 face = cascade.detectMultiScale(gray_image)
 (x, y, w, h) = face[0]
--- videoFaces.py
+++ videoFaces.py
@@ -1,18 +1,22 @@
 import cv2
 
 cascade = cv2.CascadeClassifier("Cascade.xml")
+left_eye_cascade = cv2.CascadeClassifier("LeftEyeCascade.xml")
 video = cv2.VideoCapture(0)
 
-while(True):
+while True:
     tmp, image = video.read()
     gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     face = cascade.detectMultiScale(gray_image)
 
-    for (x, y, w, h) in face:
-        cv2.rectangle(image, (x,y), (x+w, y+h), (180, 105, 255), 6)
+    for (xF, yF, wF, hF) in face:
+        eye = left_eye_cascade.detectMultiScale(gray_image)
+        eyes =[]
+        for (x, y, w, h) in eye:
+            eyes.append(x)
+        cv2.rectangle(image, (min(eyes),y), (min(eyes)+w, y+h), (180, 105, 255), 4)
     
     cv2.imshow("Video", image)
-    #cv2.waitKey(1)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
